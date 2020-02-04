0fad43a9e976801987d3d6012fa3330c29eb51f0
sumitkumarzk91 <sumitkumarzk91.com>
2020-02-04 15:28:37 +0530

    ‘9th’

---
+++ Project1.java
@@ -0,0 +1,15 @@
+package com.Project1;
+
+public class Project1 {
+	public static void main(String[] args) {
+		int a=10;
+		for(int i=0;i<10;i++) {
+						System.out.println("Helloas");
+						System.out.println(a);
+		}
+		System.out.println("Hello");
+		System.out.println("Helloasas");
+		System.out.println("Helloasas");
+	}
+
+}
