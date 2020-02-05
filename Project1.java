10879af20c46ae9b3f8cae0090b207f1690bb22f
sumitkumar676 <54640195+sumitkumar676@users.noreply.github.com>
2020-02-05 10:48:48 +0530

    Update Project1.java

--- Project1.java
+++ Project1.java
@@ -4,7 +4,7 @@ public class Project1 {
 	public static void main(String[] args) {
 		int a=10;
 		for(int i=0;i<10;i++) {
-			System.out.println("Code Changes");
+			System.out.println("Code123 Changes");
 						System.out.println("Helloas");
 						System.out.println(a);
 		}
